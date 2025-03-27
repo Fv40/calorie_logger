868ad2402a6acd48183b50e7df28da479805554f
Fv40 <deckerd70@yahoo.com>
2025-03-27 07:53:05 -0400

    temporary proxy update

--- static/javascript/serverInterface.js
+++ static/javascript/serverInterface.js
@@ -1,4 +1,4 @@
-const proxy = "https://b99c-98-15-181-20.ngrok-free.app"
+const proxy = "https://eb33-98-15-181-20.ngrok-free.app"
 const userEndpoint = `${proxy}/user/get-access`
 const calorieEndpoint = `${proxy}/calorie/add`
 let auth;
